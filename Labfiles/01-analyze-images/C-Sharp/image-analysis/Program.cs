378f7a72a0864926f20e4c5ffc3874f1ed78691e
RKiddle <asiatutors@gmail.com>
2024-11-06 19:14:04 +0700

    first commit

---
+++ Labfiles/01-analyze-images/C-Sharp/image-analysis/Program.cs
@@ -0,0 +1,107 @@
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Drawing;
+using Microsoft.Extensions.Configuration;
+using Azure;
+
+// Import namespaces
+ // Import namespaces
+ using Azure.AI.Vision.ImageAnalysis;
+
+
+namespace image_analysis
+{
+    class Program
+    {
+
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                // Get config settings from AppSettings
+                IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+                IConfigurationRoot configuration = builder.Build();
+                string aiSvcEndpoint = configuration["AIServicesEndpoint"];
+                string aiSvcKey = configuration["AIServicesKey"];
+
+                // Get image
+                string imageFile = "images/street.jpg";
+                if (args.Length > 0)
+                {
+                    imageFile = args[0];
+                }
+
+                // Authenticate Azure AI Vision client
+                // Authenticate Azure AI Vision client
+                ImageAnalysisClient client = new ImageAnalysisClient(
+                new Uri(aiSvcEndpoint),
+                new AzureKeyCredential(aiSvcKey));
+                
+                // Analyze image
+                AnalyzeImage(imageFile, client);
+
+                // Remove the background or generate a foreground matte from the image
+                await BackgroundForeground(imageFile, aiSvcEndpoint, aiSvcKey);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static void AnalyzeImage(string imageFile, ImageAnalysisClient client)
+        {
+            Console.WriteLine($"\nAnalyzing {imageFile} \n");
+
+            // Use a file stream to pass the image data to the analyze call
+            using FileStream stream = new FileStream(imageFile,
+                                                     FileMode.Open);
+
+            // Get result with specified features to be retrieved
+            // Get result with specified features to be retrieved
+            ImageAnalysisResult result = client.Analyze(
+                BinaryData.FromStream(stream),
+                VisualFeatures.Caption | 
+                VisualFeatures.DenseCaptions |
+                VisualFeatures.Objects |
+                VisualFeatures.Tags |
+                VisualFeatures.People);
+            
+            // Display analysis results
+            // Display analysis results
+            // Get image captions
+            if (result.Caption.Text != null)
+            {
+                Console.WriteLine(" Caption:");
+                Console.WriteLine($"   \"{result.Caption.Text}\", Confidence {result.Caption.Confidence:0.00}\n");
+            }
+
+            // Get image dense captions
+            Console.WriteLine(" Dense Captions:");
+            foreach (DenseCaption denseCaption in result.DenseCaptions.Values)
+            {
+                Console.WriteLine($"   Caption: '{denseCaption.Text}', Confidence: {denseCaption.Confidence:0.00}");
+            }
+
+            // Get image tags
+
+
+            // Get objects in the image
+
+
+            // Get people in the image
+
+        }
+        static async Task BackgroundForeground(string imageFile, string endpoint, string key)
+        {
+            // Remove the background from the image or generate a foreground matte
+            
+        }
+    }
+}
